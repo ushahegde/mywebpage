387398c47d0a05861ea6625a0c3f912e1b1962d2
ushahegde <hegde112233@gmail.com>
2026-04-23 19:46:42 +0530

    Modified C quiz to add descriptive questions

--- CQuiz/scriptDesc.js
+++ CQuiz/scriptDesc.js
@@ -1,64 +1,40 @@
 let questions = [];
 let selectedQuestions = [];
 let currentIndex = 0;
-let score = 0;
 
 const questionEl = document.getElementById("question");
-const optionsEl = document.getElementById("options");
+const answerEl = document.getElementById("answer");
 const explanationEl = document.getElementById("explanation");
 const nextBtn = document.getElementById("next-btn");
+const showAnswerBtn = document.getElementById("show-answer-btn");
 const resultBox = document.getElementById("result-box");
 const scoreEl = document.getElementById("score");
 
 const codeEl = document.getElementById("code");
 const explainBtn = document.getElementById("explain-btn");
-const titleEl = document.getElementById("title")
+const titleEl = document.getElementById("title");
  
-fetch("quiz.json")
+fetch("quizdesc.json")
   .then(res => res.json())
   .then(data => {
        
     questions = data;
     topics = [...new Set(questions.map(item => item.topic))];
       
-    populateTopics(topics);
-  //  startQuiz();
+    //populateTopics(topics);
+   startQuiz();
   });
 
-
-function startQuiz() {
+/*function startQuiz() {
   selectedQuestions = shuffleArray(questions).slice(0, 10);
   loadQuestion();
-}
+}*/
 
 function loadQuestion() {
   resetState();
-
   let q = selectedQuestions[currentIndex];
   questionEl.textContent = `${currentIndex + 1}. ${q.question}`;
-  let qncode=q.qncode;
-  if(qncode==null){
-      codeEl.innerText="";
-  }else{
-  //take care of < and > tags
- /* qncode = qncode.replaceAll("<","&lt;");
-  qncode = qncode.replaceAll(">","&gt;");
-  qncode = qncode.replaceAll("&","&amp;");
-  qncode = qncode.replaceAll('"',"&quot;");
-  */
-
-      codeEl.innerText=qncode;
-  }
-
-  q.options.forEach((option, index) => {
-    const btn = document.createElement("button");
-    btn.textContent = option;
-    if(option!=null && option!=""){
-       btn.onclick = () => selectAnswer(btn, index, q.correct, q.explanation);
-       optionsEl.appendChild(btn);
-    }
-  }); 
-}
+     }
 
 function selectAnswer(button, selected, correct, explanation) {
   const buttons = optionsEl.children;
@@ -87,12 +63,7 @@ function selectAnswer(button, selected, correct, explanation) {
  // explanationEl.textContent = explanation;
  // explanationEl.classList.remove("hidden");
   
-  explainBtn.onclick=function(){
-      explanationEl.classList.remove("hidden");
-      explainBtn.classList.add('hidden');
-      explanationEl.textContent=explanation;
-  }
-  }
+    }
 }
 
 nextBtn.onclick = () => {
@@ -108,22 +79,22 @@ nextBtn.onclick = () => {
 function showResult() {
   document.getElementById("quiz-box").classList.add("hidden");
   resultBox.classList.remove("hidden");
-  scoreText = score+"/"+currentIndex;
-  scoreEl.textContent = scoreText;
+  
 }
 
-function resetState() {
-  optionsEl.innerHTML = "";
+function resetState() { 
   explanationEl.classList.add("hidden");
   explainBtn.classList.add("hidden");
   nextBtn.classList.add("hidden");
-}
+  answerEl.classList.add("hidden") ;
+  showAnswerBtn.classList.remove("hidden");
+  }
 
 function shuffleArray(array) {
   return array.sort(() => Math.random() - 0.5);
 }
-function populateTopics(topics) {
-  const topicSelect = document.getElementById("topicSelect");
+function startQuiz() {
+  const topicSelect = document.getElementById("topic");
   
   // Optional: add "All" option
   const allOption = document.createElement("option");
@@ -131,7 +102,7 @@ function populateTopics(topics) {
   allOption.textContent = "All Topics";
   topicSelect.appendChild(allOption);
 
-  // topics = new Set(data.map(q => q.topic));
+  //topics = new Set(data.map(q => q.topic));
 
   topics.forEach(topic => {
     const option = document.createElement("option");
@@ -140,12 +111,30 @@ function populateTopics(topics) {
     topicSelect.appendChild(option);
   });
   topicSelect.addEventListener("change",(event)=>{
-      topic = event.target.value;
-      selectedQuestions = getRandomQuestionsByTopic(questions,topic,10);
+      const topic = event.target.value;
+      selectedQuestions = getRandomQuestionsByTopic(questions,event.target.value,10);
+      const topicLine = document.getElementById("topicline"); 
+      topicLine.classList.add("hidden");
       titleEl.textContent="Quiz - "+topic;
-      event.target.classList.add("hidden");
       loadQuestion();
   })
+  showAnswerBtn.onclick=() =>{
+  	answerEl.classList.remove("hidden");
+  	answer = selectedQuestions[currentIndex].answer;
+  	/*if(looksLikeCode(answer)){
+  	    answer=answer+"</pre>"
+  	}*/
+  	answerEl.innerText=answer;
+  	event.target.classList.add("hidden");
+  	nextBtn.classList.remove("hidden");
+  	explainBtn.classList.remove("hidden");
+  }
+  explainBtn.onclick=function(){
+      explanationEl.classList.remove("hidden");
+      explainBtn.classList.add('hidden');
+      explanationEl.textContent=selectedQuestions[currentIndex].explanation;
+  }
+
 }
  
 function getRandomQuestionsByTopic(data, topic, count = 10) {
@@ -154,4 +143,7 @@ function getRandomQuestionsByTopic(data, topic, count = 10) {
   const shuffled = [...filtered].sort(() => 0.5 - Math.random());
   return shuffled.slice(0, count);
    
+}
+function looksLikeCode(text) {
+  return /#include|main|{|}/.test(text);
 }
